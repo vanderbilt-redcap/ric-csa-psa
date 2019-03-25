101362f4cbcec079ab53f4b46b8dd8b03c92d210
Carl Reed <carl.w.reed@vumc.org>
2019-03-25 14:34:10 -0500

    added getFieldLabels function to Report class

--- report.php
+++ report.php
@@ -35,10 +35,49 @@ class Report {
 		}
 	}
 	
-	public static function getReportData($args) {
+	public static function getReportData($projectIDs) {
 		$data = [];
-		foreach ($args as $pid) {
-			$data[$pid] = \REDCap::getData($pid);
+		foreach ($projectIDs as $pid) {
+			$project = new \Project($pid);
+			$eid = $project->firstEventId;
+			$project = \REDCap::getData($pid);
+			if ($project[1][$eid]['published'] == true) {
+				echo("<pre>");
+				$labels = \Report::getFieldLabels(['pid' => $pid, 'field' => 'ct_location']);
+				print_r($labels);
+				echo("</pre>");
+				exit();
+				// we're going to re-organize the project data so it'll be easier to use on the front-end
+				$locations = [];
+				$pages = [];
+				
+				$contacts = $project;
+				$hits = $project[$pid][1]['repeat_instances'][$eid]['hits'];
+				
+				echo("<pre>");
+				print_r($contacts);
+				echo("</pre>");
+				exit();
+				foreach ($contacts as $contact) {
+					$location = $contact['ct_location'];
+					if (isset($locations[$location])) {
+						
+					} else {
+						
+					}
+				}
+				foreach ($hits as $hit) {
+					
+				}
+				
+				$data[] = [
+					'study_name' => $project->project['app_title'],
+					'locations' => $locations,
+					'pages' => $pages
+				];
+			} else {
+				unset($data[$pid]);
+			}
 		}
 		return $data;
 	}
@@ -46,9 +85,24 @@ class Report {
 	public static function makeGeneralReport($args) {
 		include 'generalReport.html';
 	}
+	
+	public static function getFieldLabels($args) {
+		$project = new \Project($args['pid']);
+		$csv = $project->metadata[$args['field']]['element_enum'];
+		$csv = explode('\n', $csv);
+		$labels = [];
+		foreach ($csv as $line) {
+			preg_match_all('/(\d+),\s+(.*)/', $line, $matches);
+			if (!empty($matches) and isset($matches[1][0])) {
+				$labels[$matches[1][0]] = $matches[2][0];
+			}
+			unset($matches);
+		}
+		return $labels;
+	}
 }
 
 $pids = \Report::getProjectIDs();
 $data = \Report::getReportData($pids);
-\Report::makeGeneralReport($data);
+// \Report::makeGeneralReport($data);
 // echo($genReport);
