ef4083e8aa00413b560ecfa9803d755fd3a5839f
Carl Reed <carlreedw@gmail.com>
2021-09-15 11:36:23 -0500

    updates config file to use the Victr_Env replacement class Victrlib

--- config.php
+++ config.php
@@ -1,23 +1,27 @@
 <?php
+use Vanderbilt\Victrlib\Env;
 if(!defined("ENVIRONMENT")) {
-	// echo('1');
-	if (is_file('/app001/victrcore/lib/Victr/Env.php')) include_once('/app001/victrcore/lib/Victr/Env.php');
-	if (class_exists("Victr_Env")) {
-		$envConf = Victr_Env::getEnvConf();
+	# Define the environment: options include "DEV", "TEST" or "PROD"
+	if (is_file('/app001/www/redcap/plugins/victrlib/src/Env.php'))
+		include_once('/app001/www/redcap/plugins/victrlib/src/Env.php');
 
-		if ($envConf[Victr_Env::ENV_CURRENT] === Victr_Env::ENV_PROD) {
+	if (class_exists("\\Vanderbilt\\Victrlib\\Env")) {
+		if (Env::isProd()) {
 			define("ENVIRONMENT", "PROD");
 			define("MASTER_PID", 91748);	// real project on prod");
-		} elseif ($envConf[Victr_Env::ENV_CURRENT] === Victr_Env::ENV_DEV) {
+		} else if (Env::isStaging()) {
 			define("ENVIRONMENT", "TEST");
 			define("MASTER_PID", 1297);
 		}
-	} elseif (gethostname() == 'VICTRWD-83SJHQ2') {
-		define("ENVIRONMENT", "DEV");
-		define("MASTER_PID", 28);
 	} else {
 		define("ENVIRONMENT", "DEV");
-		define("MASTER_PID", 19);
+		if (gethostname() == 'VICTRWD-83SJHQ2') {
+			define("ENVIRONMENT", "DEV");
+			define("MASTER_PID", 28);
+		} else {
+			define("ENVIRONMENT", "DEV");
+			define("MASTER_PID", 19);
+		}
 	}
 }
 
